d67cd17969d6704f92da4e7e8937eb80cb80a6d8
Sergei Driga <drigaserge@gmail.com>
2023-03-28 01:29:27 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String login = "gfhgwunbe";
+        String password = "ghemKne3";
+        String confirmPassword = "rjbguje";
+        checkLogInCredentials(login, password, confirmPassword);
+    }
+
+    private static boolean checkLogInCredentials(String login, String password, String confirmPassword) {
+        boolean credentialsCorrect = true;
+        if (login.length() > 20 || password.length() > 20) {
+            credentialsCorrect = false;
+        }
+
+        return credentialsCorrect;
     }
 }
